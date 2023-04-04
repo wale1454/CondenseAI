47f57d9f7234789ce43206fb5385c7983e8fb442
Wale Olowonyo F <31905212+wale1454@users.noreply.github.com>
2023-04-04 10:29:18 +0100

    Added useful comments

--- app.py
+++ app.py
@@ -1,35 +1,34 @@
+# Import the necessary modules
 import os
-
-# import openai
 from flask import Flask, redirect, render_template, request, url_for
 
 app = Flask(__name__)
 
-
 from goose3 import Goose
 import cohere
 
+# Define the endpoint for the root URL path ("/")
 @app.route("/", methods=("GET", "POST"))
 def index():
 
+    # Initialize variables for the article name and summary
     articleName = None
     summary = None
 
     if request.method == "POST":
         urll = request.form["animal"]
 
+        # Use Goose to extract article information from the URL
         g = Goose()
         article = g.extract(url=urll)
 
+        # Extract the article name and body
         articleName = article.title
         articleBody = article.cleaned_text
 
-        # Summary
-        articleBody2 = articleBody.replace("\n", " ")
+        
+        # Use the Cohere API to summarize the article
         apiKey = os.getenv('COHERE_APIKEY', 'default_value')
-
-#         apiKey = 'INSERT-API-KEY'
-
         co = cohere.Client(apiKey)
 
         response = co.summarize( 
@@ -39,10 +38,11 @@ def index():
             extractiveness='medium'
         )
 
+        # Store the summary in the summary variable
         summary = response.summary
                 
 
-
+    # Renders the HTML template with the article name and summary included.
     return render_template("index.html", result=articleName, result5 =summary)     
 
 
