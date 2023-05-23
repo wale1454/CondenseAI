72444c7445656e26a74578baa184fe6d97d1d882
Wale Olowonyo F <31905212+wale1454@users.noreply.github.com>
2023-05-23 21:34:15 +0100

    Added the endpoint for the chome extension to call.

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
 # Import the necessary modules
 import os
-from flask import Flask, redirect, render_template, request, url_for
-
-app = Flask(__name__)
-
+from flask import Flask, redirect, render_template, request, url_for, jsonify
 from goose3 import Goose
 import cohere
+
+app = Flask(__name__)
 
 # Define the endpoint for the root URL path ("/")
 @app.route("/", methods=("GET", "POST"))
@@ -43,9 +42,43 @@ def index():
 
         # Store the summary in the summary variable
         summary = response.summary
-                
-
+               
     # Renders the HTML template with the article name and summary included.
     return render_template("index.html", result=articleName, result5 =summary, fullArticle= articleBody )     
 
+
+@app.route("/pro", methods=("GET", "POST"))
+def process_text():
+    articleURL = request.args.get('articleURL')
+
+    g = Goose()
+    article = g.extract(url=articleURL)
+
+    articleName = article.title
+    articleBody = article.cleaned_text
+
+    number = 42
+
+    # Use the Cohere API to summarize the article
+    apiKey = os.getenv('COHERE_APIKEY', 'default_value')
+    co = cohere.Client(apiKey)
+
+    response = co.summarize( 
+        text = articleBody,
+        format='paragraph',
+        temperature=0.4,
+        model='summarize-xlarge', 
+        length='long',
+    )
+
+    # Store the summary in the summary variable
+    summary = response.summary
+         
+    gh = { "Summary": summary,
+
+          "Title":articleName, 
+          "result":number,
+        }
+
+    return jsonify(gh)
 
